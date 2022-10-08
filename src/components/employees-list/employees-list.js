d74720a645ba267f159808324068a81e6a113711
Sergiy Khomchenko <dmtrmsklnk@gmail.com>
2022-10-08 23:32:52 +0300

    add tansitionGroup

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,15 +1,23 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.scss';
-
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 const EmployeesList = ({data, remove, onToggle}) => {
   
-   const res = data.map(({id, ...elem}) => {
-      return <EmployeesListItem {...elem} key={id} remove = {() => remove(id)} onToggle = {(e)=> onToggle(id, e.currentTarget.getAttribute('data-toggle'))}/>
-       })
+   const res = 
+   <TransitionGroup>
+     {
+    data.map(({id, ...elem}) => 
+    <CSSTransition key={id} timeout={500} classNames='post'>
+      <EmployeesListItem {...elem}  remove = {() => remove(id)} onToggle = {(e) => onToggle(id, e.currentTarget.getAttribute('data-toggle'))}/>
+    </CSSTransition>
+      )
+    }
+   </TransitionGroup>
+     
    
     return (
         <ul className="app-list list-group">
-            {res}
+          {res}         
         </ul>
     )
 }
